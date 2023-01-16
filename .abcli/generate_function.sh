2e11d9df716c08167c119bc5d49d68a1ad7e8d2b
kamangir <arash@kamangir.net>
2023-01-15 19:27:46 -0800

    validating openai generate video - kamangir/bolt#741

--- .abcli/generate_function.sh
+++ .abcli/generate_function.sh
@@ -14,14 +14,26 @@ function openai_generate_function() {
 
     abcli_log "openai: generate: image: \"$sentence\" -[$prev_filename.png ${@:5}]-> $filename.png"
 
-    local command_line="curl https://api.openai.com/v1/images/generations \
-        -H 'Content-Type: application/json' \
-        -H 'Authorization: Bearer $OPENAI_API_KEY' \
-        -d '{
-        \"prompt\": \"$sentence\",
-        \"n\": 1,
-        \"size\": \"${height}x${width}\"
-        }' >> ./raw/$filename.json"
+    if [ -z "$prev_filename" ] ; then
+        # https://beta.openai.com/docs/api-reference/images/create
+        local command_line="curl https://api.openai.com/v1/images/generations \
+            -H 'Content-Type: application/json' \
+            -H 'Authorization: Bearer $OPENAI_API_KEY' \
+            -d '{
+            \"prompt\": \"$sentence\",
+            \"n\": 1,
+            \"size\": \"${height}x${width}\"
+            }' > ./raw/$filename.json"
+    else
+        # https://beta.openai.com/docs/api-reference/images/create-edit?lang=curl
+        local command_line="curl https://api.openai.com/v1/images/edits \
+            -H 'Authorization: Bearer $OPENAI_API_KEY' \
+            -F image='@./raw/$prev_filename.png' \
+            -F prompt=\"$sentence\" \
+            -F n=1 \
+            -F size=\"${height}x${width}\" \
+            > ./raw/$filename.json"
+    fi
 
     abcli_log "⚙️  $command_line"
     if [ "$dryrun" == 1 ] ; then
--- openai_cli/__init__.py
+++ openai_cli/__init__.py
@@ -1,3 +1,3 @@
 NAME = "openai_cli"
 
-VERSION = "2.12.1"
+VERSION = "2.15.1"
