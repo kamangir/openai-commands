21bed5388d4a58883b2f9555de0843e5dde7eb34
kamangir <arash@kamangir.net>
2023-03-05 19:24:53 -0800

    DALL-E += branch - kamangir/bolt#748

--- openai_cli/DALLE/canvas.py
+++ openai_cli/DALLE/canvas.py
@@ -1,8 +1,9 @@
 from io import BytesIO
 from PIL import Image
-import random
+from enum import Enum
 import requests
 import numpy as np
+from abcli import file
 import abcli.logging
 import logging
 
@@ -13,46 +14,36 @@ logger = logging.getLogger()
 class Canvas(object):
     def __init__(
         self,
-        canvas_width=2048,
-        canvas_height=1024,
-        brush_width=256,
-        brush_height=256,
+        width=4096,
+        height=2048,
         verbose=False,
         demo_mode=False,
     ):
         self.verbose = verbose
         self.demo_mode = demo_mode
 
-        self.canvas_width = canvas_width
-        self.canvas_height = canvas_height
+        self.width = width
+        self.height = height
 
         self.image = Image.new(
             "RGB",
-            (self.canvas_width, self.canvas_height),
+            (self.width, self.height),
             (0, 0, 0),
         )
         self.mask = Image.new(
             "L",
-            (self.canvas_width, self.canvas_height),
+            (self.width, self.height),
             (0,),
         )
 
-        self.brush_width = brush_width
-        self.brush_height = brush_height
-
-        self.init_cursor()
-
-        self.horizontal_brush_move = 1
-        self.vertical_brush_move = 1
-
-    def generate(self, prompt):
+    def generate(self, brush, prompt):
         import openai
 
         box = (
-            self.cursor[0] - self.brush_width // 2,  # left,
-            self.cursor[1] - self.brush_height // 2,  # top,
-            self.cursor[0] + self.brush_width // 2,  # right,
-            self.cursor[1] + self.brush_height // 2,  # bottom,
+            brush.cursor[0] - brush.width // 2,  # left,
+            brush.cursor[1] - brush.height // 2,  # top,
+            brush.cursor[0] + brush.width // 2,  # right,
+            brush.cursor[1] + brush.height // 2,  # bottom,
         )
 
         image_ = self.image.crop(box)
@@ -71,7 +62,7 @@ class Canvas(object):
             mask=mask_byte_array,
             prompt=prompt,
             n=1,
-            size=f"{self.brush_width}x{self.brush_height}",
+            size=f"{brush.width}x{brush.height}",
         )
 
         image_url = response["data"][0]["url"]
@@ -88,7 +79,7 @@ class Canvas(object):
         self.mask.paste(
             Image.new(
                 "L",
-                (self.brush_width, self.brush_height),
+                (brush.width, brush.height),
                 (255,),
             ),
             box,
@@ -99,72 +90,38 @@ class Canvas(object):
 
             clear_output(wait=True)
 
-            image = Image.new("RGB", (3 * self.brush_width, self.brush_height))
+            image = Image.new("RGB", (3 * brush.width, brush.height))
             image.paste(image_, (0, 0))
-            image.paste(mask_, (self.brush_width, 0))
-            image.paste(image__, (2 * self.brush_width, 0))
+            image.paste(mask_, (brush.width, 0))
+            image.paste(image__, (2 * brush.width, 0))
             display(image)
 
-            image = Image.new("RGB", (2 * self.canvas_width, self.canvas_height))
+            image = Image.new("RGB", (2 * self.width, self.height))
             image.paste(self.mask, (0, 0))
-            image.paste(self.image, (self.canvas_width, 0))
+            image.paste(self.image, (self.width, 0))
             display(image)
 
         return self
 
-    def init_cursor(self):
-        self.cursor = (
-            self.canvas_width // 2,
-            self.canvas_height // 2,
-        )
+    def save(self, filename):
+        indices = np.nonzero(np.array(self.mask) == 255)
 
-        if self.verbose:
-            self.log()
+        left = indices[1].min()
+        top = indices[0].min()
+        right = indices[1].max()
+        bottom = indices[0].max()
 
-    def move_cursor(self):
-        self.cursor = (
-            min(
-                self.canvas_width - self.brush_width,
-                max(
-                    self.brush_width,
-                    int(
-                        random.uniform(
-                            -self.horizontal_brush_move * self.brush_width,
-                            self.horizontal_brush_move * self.brush_width,
-                        )
-                        + self.cursor[0]
-                    ),
-                ),
-            ),
-            min(
-                self.canvas_height - self.brush_height,
-                max(
-                    self.brush_height,
-                    int(
-                        random.uniform(
-                            -self.vertical_brush_move * self.brush_height,
-                            self.vertical_brush_move * self.brush_height,
-                        )
-                        + self.cursor[1]
-                    ),
-                ),
-            ),
-        )
+        box = (left, top, right, bottom)
 
-        if self.verbose:
-            self.log()
+        mask = self.mask.crop(box)
+        image = self.image.crop(box)
 
-        return self
+        image.save(filename)
+        mask.save(file.add_postfix(filename, "mask"))
 
-    def log(self):
-        logger.info(
-            "Canvas: {:,}x{:,}, brush: {:,}x{:,}, @ {:,}x{:,} - {:.2f}%".format(
-                self.canvas_height,
-                self.canvas_width,
-                self.brush_width,
-                self.brush_height,
-                self.cursor[1],
-                self.cursor[0],
-                np.array(self.mask).mean() / 255 * 100,
-            )
-        )
+        if self.demo_mode:
+            from IPython.display import display, clear_output
+
+            clear_output(wait=True)
+            display(mask)
+            display(image)
--- openai_cli/__init__.py
+++ openai_cli/__init__.py
@@ -1,3 +1,3 @@
 NAME = "openai_cli"
 
-VERSION = "2.31.1"
+VERSION = "2.32.1"
