2e459580f5a160afa27ddbc78ffa4eab54765f40
kamangir <arash@kamangir.net>
2023-07-03 12:37:21 -0700

    test coverage completion - kamangir/bolt#kamangir/bolt#746

--- openai_cli/__init__.py
+++ openai_cli/__init__.py
@@ -1,3 +1,3 @@
 NAME = "openai_cli"
 
-VERSION = "2.131.1"
+VERSION = "2.132.1"
---
+++ openai_cli/tests/test_ai_function_py.py
@@ -0,0 +1,17 @@
+from openai_cli.completion.functions.python import ai_function_py
+from openai_cli.completion.prompts.structured import structured_prompt
+
+
+def test_ai_function():
+    prompt = structured_prompt(
+        inputs=["a number"],
+        returns=["that number plus 12"],
+        requirements=["returns 0 if the input is less than 10"],
+    )
+
+    func = ai_function_py(
+        output_class_name="int",
+        validation_input=10,
+    )
+
+    assert func.generate(prompt.create(func.function_name))[0]
