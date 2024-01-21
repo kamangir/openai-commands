28127318355e55aa0d416d0982edc9cc33a38b81
kamangir <arash@kamangir.net>
2024-01-20 16:01:25 -0800

    test for images - kamangir/bolt#123

--- openai_cli/__init__.py
+++ openai_cli/__init__.py
@@ -3,7 +3,7 @@ from abcli.modules.cookie import cookie
 
 NAME = "openai_cli"
 
-VERSION = "2.207.1"
+VERSION = "2.209.1"
 
 DESCRIPTION = "🛠️ tools for the OpenAI API"
 
---
+++ openai_cli/tests/test_images.py
@@ -0,0 +1,27 @@
+import pytest
+from openai_cli.images.api import OpenAIImageGenerator
+from abcli import string
+from abcli.modules import objects
+
+
+@pytest.mark.parametrize(
+    "prompt",
+    [
+        ("a person flying through the streets of Vancouver."),
+    ],
+)
+def test_images(prompt):
+    generator = OpenAIImageGenerator(verbose=False)
+
+    object_name = objects.unique_object("test")
+    filename = objects.path_of(
+        f"{string.timestamp()}.png",
+        object_name,
+    )
+
+    success, _ = generator.generate(
+        prompt=prompt,
+        filename=filename,
+    )
+
+    assert success
