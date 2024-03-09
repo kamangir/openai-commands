171bc460cb9a6944d75c2fe4b564b515cdd54a5b
kamangir <arash@kamangir.net>
2024-03-08 19:43:20 -0800

    tests - kamangir/bolt#1234

--- .abcli/conda.sh
+++ .abcli/conda.sh
@@ -4,17 +4,14 @@ function openai_conda() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ]; then
-        abcli_show_usage "openai conda create_env [validate,~recreate]" \
+        abcli_show_usage "openai conda create_env [~recreate]" \
             "create conda environment."
-        abcli_show_usage "openai conda validate" \
-            "validate conda environment."
         return
     fi
 
     if [ "$task" == "create_env" ]; then
         local options=$2
         local do_recreate=$(abcli_option_int "$options" recreate 1)
-        local do_validate=$(abcli_option_int "$options" validate 0)
 
         local environment_name=openai
 
@@ -28,24 +25,8 @@ function openai_conda() {
         abcli_plugins install nbs
         abcli_plugins install aiart
 
-        pip3 install pymysql==0.10.1
-        pip3 install pytest
-        pip3 install opencv-python
-        pip3 install scikit-image
-        pip3 install matplotlib
-        pip3 install boto3
-        pip3 install bs4
+        pip3 install -r requirements.txt
 
-        # https://platform.openai.com/docs/quickstart?context=python
-        pip3 install --upgrade openai
-
-        [[ "$do_validate" == 1 ]] && openai_conda validate
-
-        return
-    fi
-
-    if [ "$task" == validate ]; then
-        echo "wip"
         return
     fi
 
