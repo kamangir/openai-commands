c56268e7f2d6b75d162d93b90c164f2aab2787f0
kamangir <arash@kamangir.net>
2024-08-08 17:44:26 -0700

    @litrev refactors - kamangir/bolt#746

--- openai_commands/literature_review/combination.py
+++ openai_commands/literature_review/combination.py
@@ -1,4 +1,8 @@
+import glob
+import pandas as pd
 from blueness import module
+from abcli import file
+from abcli.modules import objects
 from openai_commands import NAME
 from openai_commands.logger import logger
 
@@ -19,6 +23,38 @@ def combine(
         )
     )
 
-    logger.info("🪄")
+    for filename_1 in glob.glob(objects.path_of("*.csv", object_name_1)):
+        filename_name_and_Extension = file.name_and_extension(filename_1)
+
+        filename_2 = objects.path_of(
+            filename_name_and_Extension,
+            object_name_2,
+        )
+
+        if not file.exist(filename_2):
+            logger.warning(
+                "missing in object-2 [{}]: {}".format(
+                    object_name_2,
+                    filename_name_and_Extension,
+                )
+            )
+            continue
+
+        success_1, df_1 = file.load_dataframe(filename_1)
+        success_2, df_2 = file.load_dataframe(filename_2)
+        if not success_1 or not success_2:
+            logger.warning("bad file: {}".format(filename_name_and_Extension))
+            continue
+
+        df = pd.merge(df_1, df_2, left_index=True, right_index=True)
+
+        if not file.save_csv(
+            objects.path_of(
+                filename_name_and_Extension,
+                object_name_2,
+            ),
+            df,
+        ):
+            return False
 
     return True
